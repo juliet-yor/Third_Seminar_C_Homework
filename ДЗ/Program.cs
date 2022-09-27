b0766ff6b489df9e9edc988de9ae841d87886ee5
Julia <juliet.yor@gmail.com>
2022-09-27 14:47:45 +0300

    Решила ДЗ

---
+++ ДЗ/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Console.WriteLine("Введите пятизначное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n / 10000 == n % 10 && n / 1000 % 10 == n / 10 % 10){
+//     Console.WriteLine("палиндром");
+// }
+// else{
+//     Console.WriteLine("не палиндром");
+// }
+
+
+//Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// Console.WriteLine("Введите x1: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("y1: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("z1: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите x2: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите y2: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите z2: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+// double result = Math.Sqrt(Math.Pow((x2-x1),2) +Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2));
+// Console.WriteLine(result);
+
+
+// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n; i++){
+//     Console.Write($"{Math.Pow(i,3)} ");
+// }
